8433118966b6e3e2715a38839368e97f67cf4ab4
Arturo <arrome10@gmail.com>
2019-03-24 11:02:52 -0500

    quinto version

--- tablas_ddl.sql
+++ tablas_ddl.sql
@@ -1,10 +1,10 @@
+-- modificaicon en rama alterna
 -- creacion de tablas del data warehouse
 create table clientes (
 nombre varchar2(50),
 dni char(8)
 )
-tablespace tmp;
-
+ablespace tmp;
 commit;:
 -- cuarto cambio
 
